03477d253c57c154bff062d141fe9ef7f7222f13
Marcelo Correia <marcelo@correia.io>
2018-10-05 15:14:54 +1000

    added concourse-dev

--- concourse-dev.sh
+++ concourse-dev.sh
@@ -2,12 +2,22 @@
 
 set -e
 
-cd $HOME
-mkdir concourse-dev
-cd concourse-dev
-curl https://concourse-ci.org/docker-compose.yml -O
-sed -e's/CONCOURSE_EXTERNAL_URL=.*/CONCOURSE_EXTERNAL_URL=http:\/\/localhost:8080/g' docker-compose.yml
+CONCOURSE_DEV_HOME=$HOME/concourse-dev
 
-docker-compose up -d
+up(){
+    docker-compose up -d
+}
+
+if [ -d "$CONCOURSE_DEV_HOME" ];then
+    echo CONCOURSE_DEV_HOME @ $CONCOURSE_DEV_HOME already exists
+    cd $CONCOURSE_DEV_HOME
+    up
+else
+    mkdir $CONCOURSE_DEV_HOME
+    cd $CONCOURSE_DEV_HOME
+    curl https://concourse-ci.org/docker-compose.yml -O
+    sed -i .bak -e 's/CONCOURSE_EXTERNAL_URL.*/CONCOURSE_EXTERNAL_URL=http:\/\/localhost:8080/g'  $CONCOURSE_DEV_HOME/docker-compose.yml
+    up
+fi
 
 
