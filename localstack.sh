110df4d9f59d53f4faad76ff655ac438e7eb9e7d
Marcelo Correia <marcelo@correia.io>
2018-10-05 12:44:26 +1000

    add localstack

--- localstack.sh
+++ localstack.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
-set -ex
+set -e
 
 
 echo "Starting localstack @ local environment"
+
 docker run -d \
     --name localstack \
     -p 443:443 \
     -p 4567-4583:4567-4583 \
     -p 4590-4593:4590-4593 \
     -p 8080:8080 \
-        localstack/localstack
+    localstack/localstack
 
